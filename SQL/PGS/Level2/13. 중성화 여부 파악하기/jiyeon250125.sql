c23fa433727d08bb6b3a57576cb7db8743f433bb
JiyeonJeong02 <wlduswjd1@gmail.com>
2025-01-25 16:51:23 +0900

    jiyeon sql 250125

---
+++ SQL/PGS/Level2/13. 중성화 여부 파악하기/jiyeon250125.sql
@@ -0,0 +1,11 @@
+-- 보호소의 동물이 중성화되었는지 아닌지 파악
+-- 중성화된 동물은 SEX_UPON_INTAKE 컬럼에 'Neutered' 또는 'Spayed'라는 단어가 들어있음
+
+-- 동물의 아이디와 이름, 중성화 여부를 아이디 순으로 조회
+-- 이때 중성화가 되어있다면 'O', 아니라면 'X'라고 표시
+SELECT ANIMAL_ID, NAME,
+    CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' 
+            OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+         ELSE 'X' END AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
