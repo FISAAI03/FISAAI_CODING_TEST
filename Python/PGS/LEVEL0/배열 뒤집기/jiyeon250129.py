40df5cb1564a24762d644c5280f34cf130651728
JiyeonJeong02 <wlduswjd1@gmail.com>
2025-01-30 16:30:27 +0900

    250130

---
+++ Python/PGS/LEVEL0/배열 뒤집기/jiyeon250129.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    return num_list[::-1]
+
+
+# def solution(num_list):
+#    num_list.reverse()
+#    return num_list
+
+
+
+# [기억해야 할 것] 리스트 뒤집기: [::-1] vs .reverse() 차이점
+# 상세 설명: 블로그 (https://chodang-corn.tistory.com/9)
---
+++ Python/PGS/LEVEL0/제곱수 판별하기/jiyeon250129.py
@@ -0,0 +1,8 @@
+def solution(n):
+    if int(n**(1/2))**2 == n: 
+        return 1
+    else:
+        return 2
+
+# def solution(n):
+#    return 1 if round(n**0.5) ** 2 == n else 2
