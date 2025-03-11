d4370dfe08f350a3c570b2f65bed8b05c28f5802
JiyeonJeong02 <wlduswjd1@gmail.com>
2025-03-11 16:56:48 +0900

    250311 python lv0, lv1 10sol

---
+++ Python/PGS/LEVEL0/[PCCE 기출문제] 3번  수 나누기/jiyeon250311.py
@@ -0,0 +1,27 @@
+number = int(input())
+
+answer = 0
+
+while number > 0:
+    answer += number % 100
+    number //= 100
+
+print(answer)
+
+#include <iostream>
+
+#using namespace std;
+
+# int main(void) {
+#     int number;
+#     cin >> number;
+    
+#     int answer = 0;
+    
+#     for(int i=0; number > 0; i++){
+#         answer += number % 100;
+#         number /= 100;
+#     }
+#     cout << answer << endl;
+#     return 0;
+# }
